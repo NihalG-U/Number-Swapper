f9e9da323c346054a03805c6b498d550c7c3e320
Nihal Giri Upadhyaya <nihalupadhyaya2011@gmail.com>
2025-05-16 14:35:58 +0530

    Add files via upload

---
+++ NumberSwapper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d;
+    int o = 0;
+    printf("enter two numbers ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    printf(" Swapping the numbers\n");
+    
+    d = a-o;
+    c = b-o;
+    
+    printf("the swapped numbers are : a = %d\n", c );
+    printf("and b = %d", d);
+
+    return 0;
+}
